2be9413473eb811de54967863acf46c3007094eb
panw3i <panw3i@gmail.com>
2023-04-13 10:02:17 +0800

    Update VerifedAndReputablePolicyState.bat

--- VerifedAndReputablePolicyState.bat
+++ VerifedAndReputablePolicyState.bat
@@ -1,1 +1,4 @@
+REM 添加以下注册表项可以关闭 "VerifedAndReputablePolicyState"：
+REM HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\VerifiedAndReputableFileDownload
+
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "VerifiedAndReputableFileDownload" /t REG_DWORD /d 0 /f
